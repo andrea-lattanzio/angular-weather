4702386d92a545cb2601044492dfe84e5e0ef0d9
Ltensio <76816528+Ltensio@users.noreply.github.com>
2024-10-29 11:18:09 +0100

    feat: add initial components and services for setup

---
+++ src/app/components/temperature-graph/temperature-graph.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-temperature-graph',
+  standalone: true,
+  imports: [],
+  templateUrl: './temperature-graph.component.html',
+  styleUrl: './temperature-graph.component.scss'
+})
+export class TemperatureGraphComponent {
+
+}
