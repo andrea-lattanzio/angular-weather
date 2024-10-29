c3e69db73ceb3bf9f35960479bbbe8df4a908eae
Ltensio <76816528+Ltensio@users.noreply.github.com>
2024-10-29 11:23:35 +0100

    feat: add routing and sidebar component to app component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'weather',
+    loadComponent: () => import('./pages/weather/weather.component').then((m) => m.WeatherComponent),
+  },
+  {
+    path: 'favorites',
+    loadComponent: () => import('./pages/favorites/favorites.component').then((m) => m.FavoritesComponent),
+  },
+  {
+    path: '**',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+];
