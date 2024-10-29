f2705901261060e99623c9562c4215a787ccc38f
Ltensio <76816528+Ltensio@users.noreply.github.com>
2024-10-29 12:37:33 +0100

    feature: add weather info component

--- src/app/pages/weather/weather.component.ts
+++ src/app/pages/weather/weather.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { WeatherInfoComponent } from '../../components/weather-info/weather-info.component';
+import { SearchbarComponent } from '../../components/searchbar/searchbar.component';
+import { TemperatureGraphComponent } from '../../components/temperature-graph/temperature-graph.component';
 
 @Component({
   selector: 'app-weather',
   standalone: true,
-  imports: [],
+  imports: [SearchbarComponent, WeatherInfoComponent, TemperatureGraphComponent],
   templateUrl: './weather.component.html',
   styleUrl: './weather.component.scss'
 })
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{html,ts}",
+    './pages/**/*.{html,ts}',
+    './components/**/*.{html,ts}',
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
 }
