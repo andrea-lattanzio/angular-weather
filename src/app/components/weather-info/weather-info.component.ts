3ddeaa3fe140d465347b202ab375a7a4d1d5ce43
Ltensio <76816528+Ltensio@users.noreply.github.com>
2024-10-30 17:22:26 +0100

    feat: add or remove to favorites from weather-info.component
    implemented local-storage.service for crud operations on locastorage

--- src/app/components/weather-info/weather-info.component.ts
+++ src/app/components/weather-info/weather-info.component.ts
@@ -1,29 +1,47 @@
-import { Component, inject, Input, OnChanges, SimpleChanges } from '@angular/core';
+import {
+  Component,
+  inject,
+  Input,
+  OnChanges,
+  SimpleChanges,
+} from '@angular/core';
 import { LocationInfoSimplified } from '../../interfaces/location';
 import { WeatherService } from '../../services/weather.service';
 import { CommonModule } from '@angular/common';
+import { LocalStorageService } from '../../services/local-storage.service';
 
 @Component({
   selector: 'app-weather-info',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './weather-info.component.html',
-  styleUrl: './weather-info.component.scss'
+  styleUrl: './weather-info.component.scss',
 })
 export class WeatherInfoComponent implements OnChanges {
   weatherSrv = inject(WeatherService);
   weatherInfo$ = this.weatherSrv.weatherInfo$;
-  
+  localStorageSrv = inject(LocalStorageService);
+  isFavorite!: boolean;
+
   @Input() location!: LocationInfoSimplified | undefined;
 
   ngOnChanges(changes: SimpleChanges): void {
-    if(!changes['location'].currentValue)
-      return;
-    
+    if (!changes['location'].currentValue) return;
+
     this.getWeatherInfo();
+    this.isFavorite = this.localStorageSrv.isFavorite(this.location!);
   }
 
   getWeatherInfo(): void {
     this.weatherSrv.getLocationWeather(this.location!);
+  }
+
+  handleFavoriteClick(): void {
+    if(this.localStorageSrv.isFavorite(this.location!)){
+      this.localStorageSrv.remove(this.location!);
+    } else {
+      this.localStorageSrv.add(this.location!);
+    }
+    this.isFavorite = !this.isFavorite;
   }
 }
--- src/app/services/local-storage.service.ts
+++ src/app/services/local-storage.service.ts
@@ -1,9 +1,41 @@
 import { Injectable } from '@angular/core';
+import { LocationInfoSimplified } from '../interfaces/location';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class LocalStorageService {
+  private favoriteKey = 'favorites';
+  private favorites: LocationInfoSimplified[] = [];
 
-  constructor() { }
+  constructor() {
+    const favoritesJson = localStorage.getItem('favoriteLocations');
+    this.favorites = favoritesJson ? JSON.parse(favoritesJson) : [];
+  }
+
+  add(location: LocationInfoSimplified): void {
+    this.favorites.push(location);
+    localStorage.setItem(this.favoriteKey, JSON.stringify(this.favorites));
+  }
+
+  isFavorite(location: LocationInfoSimplified): boolean {
+    return this.favorites.some(
+      (fav) =>
+        fav.latitude === location.latitude &&
+        fav.longitude === location.longitude
+    );
+  }
+
+  remove(location: LocationInfoSimplified): void {
+    const index = this.favorites.findIndex(
+      (fav) =>
+        fav.latitude === location.latitude &&
+        fav.longitude === location.longitude
+    );
+
+    if (index !== -1) {
+      this.favorites.splice(index, 1);
+      localStorage.setItem(this.favoriteKey, JSON.stringify(this.favorites));
+    }
+  }
 }
